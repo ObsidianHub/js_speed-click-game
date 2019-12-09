92a4ee3b8d4f97d57334e98648f5c321991f4d93
Denny Pearce <obsidian.y.dragon@gmail.com>
2019-12-09 15:13:15 +0200

    initialize variables

---
+++ index.js
@@ -0,0 +1,13 @@
+let $start = document.querySelector('#start'),
+    $game = document.querySelector('#game'),
+    $time = document.querySelector('#time'),
+    $result = document.querySelector('#result'),
+    $timeHeader = document.querySelector('#time-header'),
+    $resultHeader = document.querySelector('#result-header'),
+    $gameTime = document.querySelector('#game-time');
+
+let colors = ['red', 'blue', 'green', 'yellow', 'pink', 'purple', 'grey'];
+
+let score = 0;
+let isGameStarted = false;
+
