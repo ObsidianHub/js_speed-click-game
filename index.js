1baf85fcea3032a206f86782ecfdcb960d7c55d1
Denny Pearce <obsidian.y.dragon@gmail.com>
2019-12-09 15:16:33 +0200

    add basic function yet & realize render function

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ function show($el) {
 function hide($el) {
     $el.classList.add('hide');
 }
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function renderBox() {
+    $game.innerHTML = '';
+    var box = document.createElement('div');
+    var boxSize = getRandom(30, 100);
+    var gameSize = $game.getBoundingClientRect();
+    var maxTop = gameSize.height - boxSize;
+    var maxLeft = gameSize.width - boxSize;
+    var randomColorIndex = getRandom(0, colors.length);
+
+    box.style.height = box.style.width = boxSize + 'px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = colors[randomColorIndex];
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true');
+
+    $game.insertAdjacentElement('afterbegin', box);
+}
