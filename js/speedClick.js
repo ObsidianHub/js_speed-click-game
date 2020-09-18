664d7112bf369a1443c47920116f67701811866f
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-09-18 19:00:45 +0300

    create base variables

---
+++ js/speedClick.js
@@ -0,0 +1,7 @@
+let $start = document.querySelector("#start"),
+  $game = document.querySelector("#game"),
+  $time = document.querySelector("#time"),
+  $result = document.querySelector("#result"),
+  $timeHeader = document.querySelector("#time-header"),
+  $resultHeader = document.querySelector("#result-header"),
+  $gameTime = document.querySelector("#game-time");
